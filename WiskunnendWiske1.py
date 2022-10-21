fb96b5b0e3eb43edd5df939ff6a960655d29ea44
WilliamPopmie <41124307+williamopmie@users.noreply.github.com>
2022-10-22 00:07:42 +0200

    Updated Code

--- WiskunnendWiske1.py
+++ WiskunnendWiske1.py
@@ -1,71 +1,20 @@
-#Imports
-import time
 import random
-from statistics import mean
 
-#Define global variables
-totalQuestionsAsked = 0
-totalRandNums = []
-totalPos = []
+sims = 100_000
 
-simulations = 100000
+#SHORT, BUT INEFFICIENT
+def Solve(pos=5, cnt=0):
+    if pos == 1: return cnt
+    return Solve((lambda x: x - 1 if random.randint(0,9) < 2*(x-1) else 5)(pos), cnt+1)
 
-#Run a single simulation
-def SingleSim():
-    #Define global variables
-    global totalQuestionsAsked
-    
-    #Define local variables
-    pos = 5
+answ = sum([Solve() for _ in range(sims)])/sims
 
-    randNumList = []
-    posList = [5]
-
-    #Set the positions chance rates
-    posChances = {5:8, 4:6, 3:4, 2:2}
-
-    #Continuous loop until pos == 1
+#LONGER, BUT MORE EFFICIENT
+def Solve():
+    pos, cnt = 5, 0
     while pos != 1:
-        #Generate random number
-        randNum = random.randint(1, 10)
+        cnt += 1
+        pos = pos - 1 if random.randint(0,9) < 2*(pos-1) else 5
+    return cnt
 
-        #Update position
-        if randNum <= posChances[pos]: pos -= 1
-        else: pos = 5
-
-        #Update local variables
-        randNumList.append(randNum)
-        posList.append(pos)
-        totalQuestionsAsked += 1
-
-    #Update endresults
-    totalRandNums.append(mean(randNumList))
-    totalPos.append(mean(posList))
-
-#Print all endresults
-def PrintEverything():
-    print("Average Questions Asked:", totalQuestionsAsked / simulations)
-    print("Average Random Numbers Generated:", mean(totalRandNums))
-    print("Average Positions:", mean(totalPos))
-    print("Executed:", simulations, "times")
-
-def main():
-    #Find starttime of the program
-    start = time.time()
-
-    #Run the sim x amount of times
-    for _ in range(simulations):
-        SingleSim()
-    
-    #Find the endtime of the program
-    end = time.time()
-    
-    #Print Endresults
-    PrintEverything()
-
-    #Print the execution time of the program
-    print("Simulation Time:", end - start, "seconds")
-
-#Notation
-if __name__ == "__main__":
-    main()
+answ = sum([Solve() for _ in range(sims)])/sims
